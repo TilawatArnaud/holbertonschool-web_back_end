907bbfbaf24655405dbb9c8b4b63d4d123b12c32
TilawatArnaud <tilawatarnaud@gmail.com>
2025-07-28 13:27:07 +0200

    1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""This module define a function to concat two strings."""
+def concat(a: str, b: str) -> str:
+    """Return the concat of two strings."""
+    return a + b
+    
