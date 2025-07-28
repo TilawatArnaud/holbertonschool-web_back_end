72673dd8df1c837726a8297d053e7d272887dc30
TilawatArnaud <tilawatarnaud@gmail.com>
2025-07-28 13:50:00 +0200

    7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-"""This module define a function to convert a string and a float number to a tuple."""
-from typing import Tuple
+"""This module define a function to convert
+ a string and a float number to a tuple."""
+from typing import Tuple, Union
+
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return a tuple with a string and a float number."""
