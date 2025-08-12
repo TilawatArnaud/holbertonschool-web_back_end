2cad17bd39e6c25ff4ef952175a5b35307c7e37d
TilawatArnaud <tilawatarnaud@gmail.com>
2025-08-12 12:20:36 +0200

    start of the project and task 1

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module define a function to return a tuple of start and end index."""
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Returns a tuple of start and end index"""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
