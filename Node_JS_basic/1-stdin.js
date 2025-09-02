4a3c08ffe49e30a02045ab6335b3d0137bb2ca3f
TilawatArnaud <tilawatarnaud@gmail.com>
2025-09-02 12:52:36 +0200

    1-stdin.js

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,6 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
+});
