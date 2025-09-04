f42236d1a40571f0dc1cb5f5680c9b6e663900a1
TilawatArnaud <tilawatarnaud@gmail.com>
2025-09-04 19:50:37 +0200

    5-http.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,40 +1,33 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const databasePath = process.argv[2];
+const database = process.argv[2];
 
 const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
-  }
-  if (req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('This is the list of our students\n');
-
-    // save log to restore later
-    const originalLog = console.log;
-
-    // empty string to catch logs
+  } else if (req.url === '/students') {
     let output = '';
+    const originalConsoleLog = console.log;
+    console.log = (msg) => { output += `${msg}\n`; };
 
-    // temmporary log to catcht logs message and build answer
-    console.log = (message) => { output += `${message}\n`; };
-
-    countStudents(databasePath)
-      // restore original log and show answer
+    countStudents(database)
       .then(() => {
-        console.log = originalLog;
-        res.write(output.trim());
-        res.end();
+        console.log = originalConsoleLog;
+        res.end(`This is the list of our students\n${output}`);
       })
       .catch(() => {
-        // restore original log and error message
-        console.log = originalLog;
-        res.end('Cannot load the database');
+        console.log = originalConsoleLog;
+        res.end('This is the list of our students\nCannot load the database');
       });
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
   }
 });
+
 app.listen(1245);
 
 module.exports = app;
